07962f3859dc2f71f5eed929994df59bba623a67
иван кузьмич <ivankuzmic210@gmail.com>
2021-06-27 14:12:27 +0300

    End 8 module, read about keyword new

---
+++ DevIncubatorCore.OOP/Component/Base/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIncubatorCore.OOP.Component.Base
+{
+    class Animal
+    {
+        public Animal()
+        { 
+        }
+        public Animal(string name, int footNumber, bool hasMustache)
+        {
+            Name = name;
+            FootNumber = footNumber;
+            HasMustache = hasMustache;
+        }
+        public string Name { get; protected set; }
+        public int FootNumber { get; }
+        public bool HasMustache { get; }
+
+        public virtual void Voice()
+        {
+            Console.WriteLine("Voice");
+        }
+
+        public virtual void Move()
+        {
+            Console.WriteLine("Movement");
+        }
+
+        public virtual void IsHungry()
+        {
+            Console.WriteLine("Not hungry");
+        }
+    }
+}
---
+++ DevIncubatorCore.OOP/Component/Horse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DevIncubatorCore.OOP.Component.Base;
+
+namespace DevIncubatorCore.OOP.Component
+{
+    class Horse : Animal
+    {
+        public Horse()
+        {
+
+        }
+        public Horse(string name, int footNumber, bool hasMustache) 
+            : base(name, footNumber, hasMustache)
+        {
+            
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("I-go-go");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("Top-top");
+        }
+
+        public new void IsHungry()
+        {
+            Console.WriteLine("I want hay");
+        }
+    }
+}
