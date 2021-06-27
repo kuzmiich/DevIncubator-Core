e28f044043d1c5f2e93177bd3dd8eae2dc904ca3
иван кузьмич <ivankuzmic210@gmail.com>
2021-06-27 17:25:09 +0300

    Do 23 task

---
+++ DevIncubatorCore.InterfacesAbstractClasses/LaserPrinter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevIncubatorCore.InterfacesAbstractClasses.Component;
+
+namespace DevIncubatorCore.InterfacesAbstractClasses
+{
+    class LaserPrinter : Printer, IPrinter
+    {
+        public LaserPrinter(string type, bool isPresence) 
+            : base(type, isPresence)
+        {
+        }
+
+        public string StampType { get; init; }
+
+        public override void Start(string text)
+        {
+            Print(text);
+            Scan();
+        }
+
+        public void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("The scan is impossible");
+        }
+    }
+}
